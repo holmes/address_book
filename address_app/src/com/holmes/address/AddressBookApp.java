077e0ad3e2543a96c095048ff65f19d8680a7948
Jason Holmes <holmesj@yahoo-inc.com>
2011-11-03 10:48:41 -0700

    and the web dao will now talk to the server

--- address_app/src/com/holmes/address/AddressBookApp.java
+++ address_app/src/com/holmes/address/AddressBookApp.java
@@ -3,9 +3,12 @@ package com.holmes.address;
 import java.util.List;
 
 import roboguice.application.RoboApplication;
+import android.net.http.AndroidHttpClient;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 public class AddressBookApp extends RoboApplication {
 
@@ -18,6 +21,12 @@ public class AddressBookApp extends RoboApplication {
 		@Override
 		protected void configure() {
 			// i'm sure we'll need you at some point
+		}
+		
+		@Provides
+		@Singleton
+		public AndroidHttpClient getHttpClient() {
+			return AndroidHttpClient.newInstance( "Address Book App" );
 		}
 	}
 }
--- address_app/src/com/holmes/address/WebDao.java
+++ address_app/src/com/holmes/address/WebDao.java
@@ -1,36 +1,114 @@
 package com.holmes.address;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import roboguice.util.Ln;
+import android.net.http.AndroidHttpClient;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 @Singleton
 public class WebDao {
+	private static final String BASE_DOMAIN = "http://10.73.233.65:5000";
+	private static final String BASE_ADDRESS_PATH = BASE_DOMAIN + "/address/";
+	private static final String SPECIFIC_ADDRESS_PATH = BASE_DOMAIN + BASE_ADDRESS_PATH + "%d";
+	private static final Type ADDRESSES_TYPE = new TypeToken<List<Address>>() {}.getType();
+
+	@Inject
+	private AndroidHttpClient httpClient;
+
+	@Inject
+	private Gson gson;
 
 	public List<Address> getAddresses() {
-		ArrayList<Address> addresses = new ArrayList<Address>();
-		addresses.add( new Address( "123 A St", "Home" ) );
-		addresses.add( new Address( "456 B St", "Work" ) );
-		addresses.add( new Address( "789 C St", "Play" ) );
-		addresses.add( new Address( "123 D St", "Bar" ) );
-		
+		ArrayList<Address> addresses;
+
+		try {
+			HttpGet request = new HttpGet( BASE_DOMAIN );
+			HttpResponse response = httpClient.execute( request );
+			String content = EntityUtils.toString( response.getEntity() );
+
+			// ugly hack to just get Gson to just work
+			JSONObject json = new JSONObject( content );
+			JSONArray addressesArray = json.getJSONArray( "items" );
+
+			addresses = gson.fromJson( addressesArray.toString(), ADDRESSES_TYPE );
+		} catch ( Exception e ) {
+			Ln.e( e, "Unable to retrieve all addresses" );
+			addresses = new ArrayList<Address>();
+		}
+
 		return addresses;
 	}
 
-	public Address getAddress( long id ) {
-		return null;
+	public Address create( String address, String nickname ) {
+		try {
+			List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+			parameters.add( new BasicNameValuePair( "address", address ) );
+			parameters.add( new BasicNameValuePair( "nickname", nickname ) );
+			String body = URLEncodedUtils.format( parameters, "UTF-8" );
+
+			HttpPost request = new HttpPost( BASE_ADDRESS_PATH );
+			request.setEntity( new StringEntity( body ) );
+			
+			return executeForAddress( request );
+		} catch ( Exception e ) {
+			Ln.e( e, "Unable to retrieve all addresses" );
+			return new Address( "Unable to retrieve", "Unknown" );
+		}
 	}
 
-	public Address create( Address addressBook ) {
-		return addressBook;
+	public Address updateNickname( long id, String nickname ) {
+		try {
+			List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+			parameters.add( new BasicNameValuePair( "nickname", nickname ) );
+			String body = URLEncodedUtils.format( parameters, "UTF-8" );
+
+			HttpPut request = new HttpPut( String.format( SPECIFIC_ADDRESS_PATH, id ) );
+			request.setEntity( new StringEntity( body ) );
+
+			return executeForAddress( request );
+		} catch ( Exception e ) {
+			Ln.e( e, "Unable to retrieve all addresses" );
+			return new Address( "Unable to retrieve", "Unknown" );
+		}
+	}
+
+	public boolean deleteAddress( long id ) {
+		try {
+			HttpDelete request = new HttpDelete( String.format( SPECIFIC_ADDRESS_PATH, id ) );
+			HttpResponse response = httpClient.execute( request );
+			return response.getStatusLine().getStatusCode() == 200;
+		} catch ( Exception e ) {
+			Ln.e( e, "Unable to delete your address" );
+			return false;
+		}
 	}
 	
-	public Address updateNickname( long id, String nickName ) {
-		return null;
-	}
-
-	public void deleteAddress( long id ) {
-		;
+	private Address executeForAddress( HttpUriRequest request ) throws IOException {
+		HttpResponse response = httpClient.execute( request );
+		String content = EntityUtils.toString( response.getEntity() );
+		
+		return gson.fromJson( content.toString(), Address.class );
 	}
 }
